e4ba44baeb6607d7e0cbb3fc2d860c869f913ee6
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-13 15:56:33 +0530

    form page

--- src/app/adddonor/adddonor.component.ts
+++ src/app/adddonor/adddonor.component.ts
@@ -9,6 +9,35 @@ export class AdddonorComponent implements OnInit {
 
   constructor() { }
 
+  name=""
+  bloodgroup=""
+  dob=""
+  lastdonationdate=""
+  anypreviousdiseases=""
+  lastvaccinationdate=""
+  address=""
+  pincode=""
+  mobileno=""
+  email=""
+
+  readValues=()=>{
+    let data={
+      "name":this.name,
+  "bloodgroup":this.bloodgroup,
+  "dob":this.dob,
+  "lastdonationdate":this.lastdonationdate,
+  "anypreviousdiseases":this.anypreviousdiseases,
+  "lastvaccinationdate":this.lastvaccinationdate,
+  "address" :this.address,
+  "pincode":this.pincode,
+  "mobileno":this.mobileno,
+  "email":this.email
+    }
+
+    console.log(data)
+  }
+
+
   ngOnInit(): void {
   }
 
